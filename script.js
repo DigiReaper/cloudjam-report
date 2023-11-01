2a63ac98b6d6098fe39e4f66425b143837f066df
DigiReaper <adithya.rayala@gmail.com>
2023-11-01 10:38:32 +0530

    added video

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-AFRAME.registerComponent('markerhandler', {
-    init: function() {
-        // start video at click anywhere
-        document.body.addEventListener('click', function(){
-            const video = document.querySelector('a-video');
-            video.play();
-        });
-}});
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.querySelector('a-video');
+    var marker = document.querySelector('a-marker');
+    var vid = document.getElementById("greenscreenvideo");
+
+
+    // Pause video when barcode is not on the screen
+    marker.addEventListener('markerLost', function () {
+        // video.pause();
+        video.pause();
+
+    });
+
+    // Play video when barcode is on the screen
+    marker.addEventListener('markerFound', function () {
+        video.play();
+    });
+});
